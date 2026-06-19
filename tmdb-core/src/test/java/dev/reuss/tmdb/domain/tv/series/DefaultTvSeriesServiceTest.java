453a7caeace0aaae8a1e6a3b5b4184fe0d5d55ec
reussio <84527857+reussio@users.noreply.github.com>
2026-06-19 16:14:47 +0200

    Add Javadoc to TvSeriesList API Endpoints

--- tmdb-core/src/test/java/dev/reuss/tmdb/domain/tv/series/DefaultTvSeriesServiceTest.java
+++ tmdb-core/src/test/java/dev/reuss/tmdb/domain/tv/series/DefaultTvSeriesServiceTest.java
@@ -2,6 +2,7 @@ package dev.reuss.tmdb.domain.tv.series;
 
 import dev.reuss.tmdb.domain.tv.series.model.TvSeriesDetails;
 import dev.reuss.tmdb.domain.tv.series.model.TvSeriesListResponse;
+import dev.reuss.tmdb.domain.tv.series.query.TvSeriesDateListQuery;
 import dev.reuss.tmdb.domain.tv.series.query.TvSeriesListQuery;
 import dev.reuss.tmdb.query.AppendToResponse;
 import dev.reuss.tmdb.testsupport.RecordingTmdbHttpClient;
@@ -35,6 +36,44 @@ class DefaultTvSeriesServiceTest {
     }
 
     @Test
+    void airingTodayPassesDateListQuery() {
+        RecordingTmdbHttpClient http = new RecordingTmdbHttpClient();
+
+        new DefaultTvSeriesService(http).airingToday(
+                TvSeriesDateListQuery.create()
+                        .language(Language.of("de-DE"))
+                        .page(2)
+                        .timezone("Europe/Berlin")
+        );
+
+        assertLastRequest(
+                http,
+                "/tv/airing_today",
+                Map.of("language", "de-DE", "page", "2", "timezone", "Europe/Berlin"),
+                TvSeriesListResponse.class
+        );
+    }
+
+    @Test
+    void onTheAirPassesDateListQuery() {
+        RecordingTmdbHttpClient http = new RecordingTmdbHttpClient();
+
+        new DefaultTvSeriesService(http).onTheAir(
+                TvSeriesDateListQuery.create()
+                        .language(Language.of("de-DE"))
+                        .page(2)
+                        .timezone("Europe/Berlin")
+        );
+
+        assertLastRequest(
+                http,
+                "/tv/on_the_air",
+                Map.of("language", "de-DE", "page", "2", "timezone", "Europe/Berlin"),
+                TvSeriesListResponse.class
+        );
+    }
+
+    @Test
     void popularPassesListQuery() {
         RecordingTmdbHttpClient http = new RecordingTmdbHttpClient();
 
@@ -51,11 +90,34 @@ class DefaultTvSeriesServiceTest {
     }
 
     @Test
+    void topRatedPassesListQuery() {
+        RecordingTmdbHttpClient http = new RecordingTmdbHttpClient();
+
+        new DefaultTvSeriesService(http).topRated(
+                TvSeriesListQuery.create().language(Language.of("en-US")).page(2)
+        );
+
+        assertLastRequest(
+                http,
+                "/tv/top_rated",
+                Map.of("language", "en-US", "page", "2"),
+                TvSeriesListResponse.class
+        );
+    }
+
+    @Test
     void rejectsNullRequiredInputs() {
         DefaultTvSeriesService service = new DefaultTvSeriesService(new RecordingTmdbHttpClient());
 
         assertThrows(NullPointerException.class, () -> service.details(null));
         assertThrows(NullPointerException.class, () -> service.details(TvShowId.of(1399), (Language) null));
+        assertThrows(NullPointerException.class, () -> service.airingToday((Language) null));
+        assertThrows(NullPointerException.class, () -> service.airingToday((TvSeriesDateListQuery) null));
+        assertThrows(NullPointerException.class, () -> service.onTheAir((Language) null));
+        assertThrows(NullPointerException.class, () -> service.onTheAir((TvSeriesDateListQuery) null));
+        assertThrows(NullPointerException.class, () -> service.popular((Language) null));
         assertThrows(NullPointerException.class, () -> service.popular((TvSeriesListQuery) null));
+        assertThrows(NullPointerException.class, () -> service.topRated((Language) null));
+        assertThrows(NullPointerException.class, () -> service.topRated((TvSeriesListQuery) null));
     }
 }
